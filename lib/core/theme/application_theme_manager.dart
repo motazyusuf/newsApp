a2ddf18cd8e95f7e2d9e1d5246bd34ff88cff920
Motaz <motazyusuf@gmail.com>
2024-08-20 21:08:21 +0300

    init

---
+++ lib/core/theme/application_theme_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'color_palette.dart';
+
+class ApplicationThemeManager{
+  static ThemeData theme = ThemeData(
+       primaryColor:  ColorPalette.primaryColor
+
+      );
+}
