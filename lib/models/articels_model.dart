f057125ed995c2c143c95f84c1b6b906899ed1c0
Motaz <motazyusuf@gmail.com>
2024-08-26 10:27:35 +0300

    -Articles model -Single article model

---
+++ lib/models/articels_model.dart
@@ -0,0 +1,53 @@
+import 'package:news_app/models/sources_model.dart';
+
+class ArticlesModel{
+
+final String status;
+final List<SingleArticle> articles;
+
+  ArticlesModel({required this.status, required this.articles});
+
+
+  factory ArticlesModel.fromJson(Map<String, dynamic> json){
+    return ArticlesModel(
+        status: json["status"],
+        articles: (json["articles"] as List).map(
+                (article) => SingleArticle.fromJson(article)
+        ).toList()
+    );
+  }
+
+}
+
+class SingleArticle {
+
+  final SingleSource source;
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String publishedAt;
+  // final String urlToImage;
+  // final String content;
+
+  SingleArticle(
+      {required this.source,
+      required this.author,
+      required this.title,
+      required this.description,
+      required this.url,
+      required this.publishedAt});
+
+  factory SingleArticle.fromJson(Map<String, dynamic> json){
+    return SingleArticle(
+        source: SingleSource.fromJson(json["source"]),
+        author: json["author"],
+        title: json["title"],
+        description: json["description"],
+        url: json["url"],
+        publishedAt: json["publishedAt"]);
+  }
+
+
+
+}
